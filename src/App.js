429937fc8c7c299ac68e88b5a70572937e5f3477
williamjones62 <williamjones62@gmail.com>
2017-08-04 16:48:19 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MyComponent from './myComponent';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOn: true
+    };
+  }
+  toggle = () => {
+    this.setState({isOn: !this.state.isOn})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to my world!</h2>
+          <MyComponent name="Bill 😃" />
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <button onClick={this.toggle}>
+          {this.state.isOn ? 'On' : 'Off'}
+        </button>
+      </div>
+    );
+  }
+}
+
+export default App;
